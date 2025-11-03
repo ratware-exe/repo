88cdeb411c86a7cf13c7b054a18545d8e73dcd36
R🐀TWARE <238379539+ratware-exe@users.noreply.github.com>
2025-11-03 10:50:15 -0500

    Update and rename maid.lua to Maid.lua

---
+++ dependency/Maid.lua
@@ -0,0 +1,102 @@
+-- dependency/Maid.lua
+do
+    local globalEnv = (getgenv and getgenv()) or _G
+    local Signal = globalEnv.Signal
+    if not Signal then
+        local repoBase = globalEnv.RepoBase or error("Set _G.RepoBase before loading Maid.lua")
+        Signal = loadstring(game:HttpGet(repoBase .. "dependency/Signal.lua"), "@Signal.lua")()
+        globalEnv.Signal = Signal
+    end
+
+    local Maid = {}
+    Maid.ClassName = "Maid"
+
+    function Maid.new()
+        return setmetatable({ MaidTasks = {} }, Maid)
+    end
+
+    function Maid.isMaid(value)
+        return type(value) == "table" and value.ClassName == "Maid"
+    end
+
+    function Maid.__index(self, index)
+        if Maid[index] ~= nil then
+            return Maid[index]
+        end
+        return self.MaidTasks[index]
+    end
+
+    function Maid:__newindex(index, newTask)
+        if Maid[index] ~= nil then
+            error(("'%s' is reserved"):format(tostring(index)), 2)
+        end
+
+        local tasksTable = self.MaidTasks
+        local oldTask = tasksTable[index]
+        if oldTask == newTask then
+            return
+        end
+
+        tasksTable[index] = newTask
+
+        if oldTask then
+            if type(oldTask) == "function" then
+                oldTask()
+            elseif typeof(oldTask) == "RBXScriptConnection" then
+                oldTask:Disconnect()
+            elseif type(oldTask) == "table" and oldTask.Remove then
+                oldTask:Remove()
+            elseif Signal.isSignal(oldTask) then
+                oldTask:Destroy()
+            elseif typeof(oldTask) == "thread" then
+                task.cancel(oldTask)
+            elseif oldTask.Destroy then
+                oldTask:Destroy()
+            end
+        end
+    end
+
+    function Maid:GiveTask(taskObject)
+        if not taskObject then
+            error("Task cannot be false or nil", 2)
+        end
+        local taskId = #self.MaidTasks + 1
+        self[taskId] = taskObject
+        return taskId
+    end
+
+    function Maid:DoCleaning()
+        local tasksTable = self.MaidTasks
+
+        for key, taskInstance in pairs(tasksTable) do
+            if typeof(taskInstance) == "RBXScriptConnection" then
+                tasksTable[key] = nil
+                taskInstance:Disconnect()
+            end
+        end
+
+        local nextKey, nextTask = next(tasksTable)
+        while nextTask ~= nil do
+            tasksTable[nextKey] = nil
+
+            if type(nextTask) == "function" then
+                nextTask()
+            elseif typeof(nextTask) == "RBXScriptConnection" then
+                nextTask:Disconnect()
+            elseif Signal.isSignal(nextTask) then
+                nextTask:Destroy()
+            elseif type(nextTask) == "table" and nextTask.Remove then
+                nextTask:Remove()
+            elseif typeof(nextTask) == "thread" then
+                task.cancel(nextTask)
+            elseif nextTask.Destroy then
+                nextTask:Destroy()
+            end
+
+            nextKey, nextTask = next(tasksTable)
+        end
+    end
+
+    Maid.Destroy = Maid.DoCleaning
+    return Maid
+end
