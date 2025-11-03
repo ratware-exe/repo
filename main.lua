3b44dce071c101c010741983ad4272e709c52dbd
R🐀TWARE <238379539+ratware-exe@users.noreply.github.com>
2025-11-03 11:15:08 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,18 +1,25 @@
 -- main.lua
 do
-    _G.RepoBase = "https://raw.githubusercontent.com/ratware-exe/repo/main/"
-    _G.ObsidianRepoBase = "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/"
+    local GlobalEnv = (getgenv and getgenv()) or _G
+    GlobalEnv.RepoBase = "https://raw.githubusercontent.com/ratware-exe/repo/main/"
+    GlobalEnv.ObsidianRepoBase = "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/"
 
-    local loader = loadstring(game:HttpGet(_G.RepoBase .. "loader.lua"), "@loader.lua")()
+    -- (Keep _G in sync just in case)
+    _G.RepoBase = GlobalEnv.RepoBase
+    _G.ObsidianRepoBase = GlobalEnv.ObsidianRepoBase
+
+    local loader = loadstring(game:HttpGet(GlobalEnv.RepoBase .. "loader.lua"), "@loader.lua")()
 
     local featurePaths = {
+        -- If you split Shield out earlier, put it first:
+        -- "modules/Shield.lua",
         "modules/VIPServerCommands.lua",
         "modules/InfZoom.lua",
         "modules/AntiAFK.lua",
         "modules/UltraAFK.lua",
     }
 
-    for pathIndex = 1, #featurePaths do
-        loader.MountModule(featurePaths[pathIndex])
+    for i = 1, #featurePaths do
+        loader.MountModule(featurePaths[i])
     end
 end
