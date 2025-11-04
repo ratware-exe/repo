8db315fe63b294b3ced597dab4717ea920063eb2
R🐀TWARE <238379539+ratware-exe@users.noreply.github.com>
2025-11-04 16:28:53 -0500

    Update infinitezoom.lua

--- modules/infinitezoom.lua
+++ modules/infinitezoom.lua
@@ -1,4 +1,4 @@
--- modules/InfZoom.lua
+-- modules/infinitezoom.lua
 do
     return function(UI)
         local RbxService = loadstring(game:HttpGet(_G.RepoBase .. "dependency/Services.lua"), "@Services.lua")()
